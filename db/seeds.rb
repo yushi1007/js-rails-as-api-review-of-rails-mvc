6aaa7d3c3ed3e17ac8a1423e5d4ea0cac8ded336
Yu Shi <yushiys95@gmail.com>
2021-03-25 23:06:17 -0400

    rails API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
+# Examples: Hello
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
@@ -10,3 +10,5 @@ Bird.create(name: "Black-Capped Chickadee", species: "Poecile Atricapillus")
 Bird.create(name: "Grackle", species: "Quiscalus Quiscula")
 Bird.create(name: "Common Starling", species: "Sturnus Vulgaris")
 Bird.create(name: "Mourning Dove", species: "Zenaida Macroura")
+
+
